a01127785348236e95b21ac5f1af648263211925
freejoe76 <jmurphy@denverpost.com>
2015-07-14 18:44:57 -0400

    ...

--- deploy.bash
+++ deploy.bash
@@ -2,4 +2,4 @@
 source source.bash
 python verdict.py 
 ./ftp.bash --dir $REMOTE_DIR/output --host $REMOTE_HOST
-./ftp.bash --dir $REMOTE_DIR --source_dir www --host $REMOTE_HOST
+#./ftp.bash --dir $REMOTE_DIR --source_dir www --host $REMOTE_HOST
